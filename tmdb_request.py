d2a3b0ed6359dcd051731ded0ca482870242b269
Sarah Turner <sgturne@Sarahs-MBP.attlocal.net>
2021-10-27 14:58:57 -0400

    url movie id

--- tmdb_request.py
+++ tmdb_request.py
@@ -1,3 +1,5 @@
+import url.librequest
+import os 
 
  #api_key = "thing_copied_from_site"
 
@@ -7,6 +9,12 @@ api_key= f.read()
 f.close()
 #this will work the same as if directly had the api key inserted into document. so you can hide api key, which is like your password from anyone who may read your code.
 
+if not os.path.exists("json_files"):
+	os.mkdir("json_files")
 
 
-print(api_key)
+movie_id = 550
+
+response = urllib.request.urlopen("https://api.themoviedb.org/3/movie/"+ str(movie_id) + "?api_key="+ api_key)
+
+print(response)
